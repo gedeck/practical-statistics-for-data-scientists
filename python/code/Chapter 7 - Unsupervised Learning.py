b506370655ee7ada88885887ef8b3140576fbcc1
Peter Gedeck <peter.gedeck@gmail.com>
2023-05-24 09:49:03 -0400

    wip

--- python/code/Chapter 7 - Unsupervised Learning.py
+++ python/code/Chapter 7 - Unsupervised Learning.py
@@ -103,12 +103,21 @@ plt.show()
 
 ### Correspondence analysis
 
+from adjustText import adjust_text
+
 housetasks = pd.read_csv(HOUSE_TASKS_CSV, index_col=0)
 
 ca = prince.CA(n_components=2)
 ca = ca.fit(housetasks)
 
-ca.plot_coordinates(housetasks, figsize=(6, 6))
+ax = ca.row_coordinates(housetasks).plot.scatter(x=0, y=1, figsize=(6,6))
+ca.column_coordinates(housetasks).plot.scatter(x=0, y=1, ax=ax, c='C1')
+texts = []
+for idx, row in ca.row_coordinates(housetasks).iterrows():
+    texts.append(plt.text(row[0], row[1], idx))
+for idx, row in ca.column_coordinates(housetasks).iterrows():
+    texts.append(plt.text(row[0], row[1], idx, color='C1'))
+adjust_text(texts, only_move={'points':'y', 'texts':'y'}) 
 plt.tight_layout()
 plt.show()
 
@@ -393,7 +402,7 @@ print(x)
 ### Problems with Clustering Mixed Data
 
 columns = ['dti', 'payment_inc_ratio', 'home_', 'pub_rec_zero']
-df = pd.get_dummies(defaults[columns])
+df = pd.get_dummies(defaults[columns], dtype=int)
 
 scaler = preprocessing.StandardScaler()
 
